3ecd1e30b009b22b6ef3d8e87890976ad40c3a65
Tchjdaedn <43163522+tchjdaedn@users.noreply.github.com>
2018-10-13 21:01:49 -0500

    Production Ready PyBoss/main.py and PyParagraph/main.py

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,1 +1,50 @@
+#import modules
+import os
+import csv
 
+#designate data path
+csvpath = os.path.join('.','employee_data.csv')
+
+#use the file
+with open(csvpath, 'r') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=',')
+	
+	#initialize variables
+	EmpID = 0
+	Name=["First Name","Last Name"]
+	DOB = ["Year","Month","Day"]
+	SSN = ["123","12","1234"]
+	State = "State"
+	us_state_abbrev = {'State':'State','Alabama': 'AL','Alaska': 'AK','Arizona': 'AZ','Arkansas': 'AR','California': 'CA','Colorado': 'CO','Connecticut': 'CT','Delaware': 'DE','Florida': 'FL','Georgia': 'GA','Hawaii': 'HI','Idaho': 'ID','Illinois': 'IL','Indiana': 'IN','Iowa': 'IA','Kansas':'KS','Kentucky': 'KY','Louisiana': 'LA','Maine': 'ME','Maryland': 'MD','Massachusetts': 'MA','Michigan': 'MI','Minnesota': 'MN','Mississippi': 'MS','Missouri': 'MO','Montana': 'MT','Nebraska': 'NE','Nevada': 'NV','New Hampshire': 'NH','New Jersey': 'NJ','New Mexico': 'NM','New York': 'NY','North Carolina': 'NC','North Dakota': 'ND','Ohio': 'OH','Oklahoma': 'OK','Oregon': 'OR','Pennsylvania': 'PA','Rhode Island': 'RI','South Carolina': 'SC','South Dakota': 'SD','Tennessee': 'TN','Texas': 'TX','Utah': 'UT','Vermont': 'VT','Virginia': 'VA','Washington': 'WA','West Virginia': 'WV','Wisconsin': 'WI','Wyoming': 'WY',}
+	#rowcount = 0
+	
+	#begin row-by-row conversion
+	print(f"\nRunning Conversion on {csvpath}\n")
+	output_path = os.path.join(".", "Converted_records.txt")
+	with open(output_path, 'w') as textfile:
+		for row in csvreader:
+			#rowcount+=1
+			#print(rowcount)
+				if EmpID == 0:
+					EmpID = "Emp ID"
+					#print(f"{EmpID}\t{Name[0]}\t{Name[1]}\tDOB\tSSN\tState")
+					textfile.write(f"{EmpID},{Name[0]},{Name[1]},DOB,SSN,State")
+				else:
+					#Employee ID
+					EmpID = row[0]
+					#Name
+					Name = row[1].split()
+					#DOB
+					DOB = row[2].split("-")
+					#SSN
+					SSN = row[3].split("-")
+					#State
+					State = us_state_abbrev[row[4]]
+					textfile.write(f"\n{EmpID},{Name[0]},{Name[1]},{DOB[1]}/{DOB[2]}/{DOB[0]},***-**-{SSN[2]},{State}")
+					#print(f"{EmpID}\t{Name[0]}\t{Name[1]}\t{DOB[1]}/{DOB[2]}/{DOB[0]}\t***-**-{SSN[2]}\t{State}")
+	print(f"\n\nConversion output to {output_path}")
+
+
+		
+		
+		
--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,1 +1,73 @@
+#import modules
+import os
+import re
 
+#designate data path
+filepath = os.path.join('.','paragraph_2.txt')
+
+#initialize variables
+list = []
+sentence = []
+lengthcount = {1:0}
+wordcount = 0
+sentence_count = 0
+sentence_length =0
+weighted_ave =0
+raw_sentence_count = 0
+
+#use the file
+with open(filepath, 'r', encoding='utf-8') as textfile:
+    for block in textfile:
+        paragraph = re.split("(?<=[.\n!?]) +", block)
+        sentence_count = len(paragraph)
+        for sentence_num in range(0,len(paragraph)):
+            #print(f" {paragraph[sentence_num]}\t{len(paragraph)}")
+            #raw_sentence_count +=1
+            paragraph[sentence_num] = paragraph[sentence_num].replace('-',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('; ',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('>',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('<',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace(' (',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace(') ',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace(',',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('–',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('`','')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('~',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('& ',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('.',' ')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('“','')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('”','')
+            paragraph[sentence_num] = paragraph[sentence_num].replace('  ',' ')
+            sentence = paragraph[sentence_num].split(" ")
+            if len(sentence)>1:
+                sentence_length += len(sentence)
+                raw_sentence_count +=1
+            
+            #print(f"{sentence}, {len(sentence)}")
+            for word_num in range(0,len(sentence)):
+                #print(f"{len(sentence[word_num])}\t{sentence[word_num]}")
+                if len(sentence[word_num])>0:
+                    wordcount +=1
+                    check = 0
+                while check == 0:
+                    for i in lengthcount.keys():
+                        #print(f"{i}, {lengthcount[i]}")
+                        if i == len(sentence[word_num]):
+                            lengthcount[i] = int(lengthcount[i])+1
+                            check = 1
+                        if check == 0:
+                            check = 2
+                if check == 2:
+                    lengthcount[len(sentence[word_num])] = 1
+                #print(lengthcount, wordcount, sentence_count, sentence_length/sentence_count)
+    for i in lengthcount.keys():
+        #print(i,lengthcount[i])
+        weighted_ave += lengthcount[i]*i
+        #print(weighted_ave, weighted_ave/wordcount)
+    #print(raw_sentence_count)
+print(f"\nParagraph Analysis")
+print(f"------------------")
+print(f"Word count: {wordcount}")
+print(f"Sentence count: {raw_sentence_count}")
+print(f"Average Letter count: {weighted_ave/wordcount}")
+print(f"Average Sentence Length: {sentence_length/raw_sentence_count}")
